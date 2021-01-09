7a7c87051255919806c1579a5e55b8821f8f3b0b
Rushikesh Gavali <gavalirushikesh43@gmail.com>
2021-01-09 22:33:11 +0530

    Made password field encrypted to store on database

--- server.js
+++ server.js
@@ -3,53 +3,66 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("./db-config.js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const User = require("./db-models/user");
 const mongoose = require("mongoose");
+
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
 app.post("/signup", (req, res) => {
-  const { userName, email, password } = req.body;
-  User.findOne({ email: email }).exec((err, user) => {
-    if (user) {
-      res.send({ error: "The user already exists" });
-    } else {
-      const newUser = new User({
-        userName: userName,
-        email: email,
-        password: password,
-      });
-      newUser
-        .save()
-        .then((user) => {
-          const token = jwt.sign({ id: user._id }, "secretKey", {
-            expiresIn: 86400, // expires in 24 hours
-          });
-          res.status(200).send({ auth: true, token: token });
-        })
-        .catch((error) => {
-          res.status(500).send();
-        });
-    }
-  });
+    const {userName, email, password} = req.body;
+    User.findOne({email: email}).exec((err, user) => {
+        if (user) {
+            res.send({error: "The user already exists"});
+        } else {
+            bcrypt.genSalt(saltRounds, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    const newUser = new User({
+                        userName: userName,
+                        email: email,
+                        password: hash,
+                    });
+                    newUser
+                        .save()
+                        .then((user) => {
+                            const token = jwt.sign({id: user._id}, "secretKey", {
+                                expiresIn: 86400, // expires in 24 hours
+                            });
+                            res.status(200).send({auth: true, token: token});
+                        })
+                        .catch((error) => {
+                            res.status(500).send();
+                        });
+                });
+            });
+        }
+    });
 });
 
 app.post("/signIn", (req, res) => {
-  const { userName, password } = req.body;
-  User.findOne({ userName: userName }).exec((err, user) => {
-    if (user && user.password === password) {
-      const token = jwt.sign({ id: user._id }, "secretKey", {
-        expiresIn: 86400, // expires in 24 hours
-      });
-      res.status(200).send({ auth: true, token: token });
-    } else {
-      res.send({ error: "No such user exists" });
-    }
-  });
+    const {userName, password} = req.body;
+    User.findOne({userName: userName}).exec((err, user) => {
+        if (user) {
+            bcrypt.compare(password, user.password, function (err, isPasswordMatched) {
+                if (isPasswordMatched) {
+                    const token = jwt.sign({id: user._id}, "secretKey", {
+                        expiresIn: 86400, // expires in 24 hours
+                    });
+                    res.status(200).send({auth: true, token: token});
+                } else {
+                    res.send({error: "No such user exists"});
+                }
+            });
+        } else {
+            res.send({error: "No such user exists"});
+        }
+    });
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("listening on port 8080");
+    console.log("listening on port 8080");
 });
